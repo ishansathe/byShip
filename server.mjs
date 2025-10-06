24955ce8d32b07c09f44c0cfbf6f8dbd9e077e0d
Ishan Sathe <ishansatheprofessional@gmail.com>
2025-10-06 16:14:27 +0530

    Made changes to the entry.html file. Adding a bit of CSS magic to make the UI bearable and kind of easier to use. Made changes to the server.mjs to make it easier for debugging and development. (I don't feel like using nodemon)

--- server.mjs
+++ server.mjs
@@ -1,11 +1,16 @@
 import express from 'express';
+import fs from 'fs'
+
 
 const app = express();
-
 const PORT = 5194;
+// const entryFile = fs.readFileSync('./src/entry_files/entry.html', 'utf-8');
+
+app.use(express.static('./src/entry_files'))
+app.use(express.static('./src/registration_files'))
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.send(fs.readFileSync('./src/entry_files/entry.html', 'utf-8'));
 })
 
 app.listen(PORT, () => {
