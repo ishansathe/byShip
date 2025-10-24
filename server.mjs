6caf47657e32d0a4cfb6c32f8c0d58b85cb46e8f
Ishan Sathe <ishansatheprofessional@gmail.com>
2025-10-24 21:12:16 +0530

    Testing the api

--- server.mjs
+++ server.mjs
@@ -7,24 +7,24 @@ const PORT = 5194;
 
 // app.use(express.static('./public'))
 
-// app.get('/api', (req, res) => {
-//     // The name was landingPage, but got changed to 'index' for vercel issues.
-//     // res.json({ message: 'Hello from Express on Vercel!' });
-//     // res.send(fs.readFileSync('/public/index.html', 'utf-8'));
+app.get('/api', (req, res) => {
+    // The name was landingPage, but got changed to 'index' for vercel issues.
+    // res.json({ message: 'Hello from Express on Vercel!' });
+    // res.send(fs.readFileSync('/public/index.html', 'utf-8'));
 
-//     // Now doing some debugging
-//     let dirArray = (fs.readdirSync('./'));
+    // Now doing some debugging
+    let dirArray = (fs.readdirSync('./'));
 
-//     let newJsonArray = [];
-//     for (let index in dirArray) {
-//         let object = {
-//             item: dirArray[index]
-//         }
-//         newJsonArray.push(object)
-//     }
+    let newJsonArray = [];
+    for (let index in dirArray) {
+        let object = {
+            item: dirArray[index]
+        }
+        newJsonArray.push(object)
+    }
 
-//     res.json(JSON.stringify(newJsonArray))
-// })
+    res.json(JSON.stringify(newJsonArray))
+})
 
 // app.listen(PORT, () => {
 //     console.log(`Now listening for ByShip traffic at port ${PORT}`)
