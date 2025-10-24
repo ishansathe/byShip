5e7db68edf14ea62f5c1d17678cef5bd20766996
Ishan Sathe <ishansatheprofessional@gmail.com>
2025-10-24 20:28:40 +0530

    Re-added server.mjs. Apparently, server.mjs entrypoint is required when you configure the vercel deployment as an express application

---
+++ server.mjs
@@ -0,0 +1,20 @@
+import express from 'express';
+import fs from 'fs';
+
+const app = express();
+const PORT = 5194;
+// const entryFile = fs.readFileSync('./src/entry_files/entry.html', 'utf-8');
+
+// app.use(express.static('./public'))
+
+app.get('/', (req, res) => {
+    // The name was landingPage, but got changed to 'index' for vercel issues.
+    res.json({ message: 'Hello from Express on Vercel!' });
+    // res.send(fs.readFileSync('/public/index.html', 'utf-8'));
+})
+
+// app.listen(PORT, () => {
+//     console.log(`Now listening for ByShip traffic at port ${PORT}`)
+// })
+
+export default app;
