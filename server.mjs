300e008be419eb722efff14a3af87d18888e1e78
Ishan Sathe <ishansatheprofessional@gmail.com>
2025-10-24 17:35:52 +0530

    I don't remember all the changes I made, must be something

--- server.mjs
+++ server.mjs
@@ -1,6 +1,33 @@
 import express from 'express';
-import fs from 'fs'
+import fs from 'fs';
+import mysql from 'mysql2';
 
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    password: '13578642',
+    database: 'pets'
+})
+
+// Callback function runs asynchronously.
+connection.connect((err)=> {
+    if(err){
+        console.error("Error connecting to mySQL database: ", err);
+        return;
+    }
+    console.log("Connection to MySQL database was successful. ID is: ", connection.threadId);
+})
+
+// connection.query('select * from cats', 
+//     // Calls this function when a response is to be received from database.
+//     (err, results, fields) => {
+//         if(err){
+//             console.error("Error when performing the query:", err);
+//             return;
+//         }
+//         console.log('Results of query', results);
+// });
 
 const app = express();
 const PORT = 5194;
@@ -11,14 +38,14 @@ app.use(express.static('./src/registration_files'))
 app.use(express.static('./src/confirmation_files'))
 
 app.get('/', (req, res) => {
-    res.send(fs.readFileSync('./src/entry_files/entry.html', 'utf-8'));
+    res.send(fs.readFileSync('./src/landingPage.html', 'utf-8'));
 })
 
 app.get('/register', (req, res) => {
     res.send(fs.readFileSync('./src/registration_files/register.html', 'utf-8'));
 })
 
-app.get('/await_confirm', (req, res) => {
+app.post('/await_confirm', (req, res) => {
     res.send(fs.readFileSync('./src/confirmation_files/waiting_confirmation.html', 'utf-8'));
 })
 
---
+++ testEmailSend.mjs
@@ -0,0 +1,28 @@
+import MailService from "@sendgrid/mail";
+
+import dotenv from 'dotenv';
+// dotenv.config();
+
+import { configDotenv } from "dotenv";
+
+configDotenv();
+
+
+MailService.setApiKey(process.env.SENDGRID_API_KEY)
+
+const msg = {
+    to: 'ishan.sathe@somaiya.edu',
+    from: 'ishansatheprofessional@gmail.com',
+    subject: 'test email',
+    text: 'This email web api provider works. This one was from Send Grid/Twilio.',
+    html: '<u>you can also add <strong>html</strong> code if you want to.</u>'
+};
+
+// MailService
+//     .send(msg)
+//     .then(() => {
+//         console.log("Sent the email!");
+//     })
+//     .catch((err) => {
+//         console.error(err);
+//     })
