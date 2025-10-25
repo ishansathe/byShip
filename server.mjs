76e95c91bab50d3d83c3ace7f6abfba13caaf865
Ishan Sathe <ishansatheprofessional@gmail.com>
2025-10-25 11:00:13 +0530

    Updated the design. Also added design for mobile

--- server.mjs
+++ server.mjs
@@ -5,31 +5,17 @@ const app = express();
 const PORT = 5194;
 // const entryFile = fs.readFileSync('./src/entry_files/entry.html', 'utf-8');
 
-// app.use(express.static('./public'))
+app.use(express.static('./public'))
 
 app.get('/', (req, res) => {
     // The name was landingPage, but got changed to 'index' for vercel issues.
     // res.json({ message: 'Hello from Express on Vercel!' });
-    // res.send(fs.readFileSync('/public/index.html', 'utf-8'));
-
-    // Now doing some debugging
-    // let dirArray = (fs.readdirSync('./'));
-
-    // let newJsonArray = [];
-    // for (let index in dirArray) {
-    //     let object = {
-    //         item: dirArray[index]
-    //     }
-    //     newJsonArray.push(object)
-    // }
-    // res.json(JSON.stringify(newJsonArray))
-
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.send(fs.readFileSync('./public/index.html', 'utf-8'));
 })
 
-// app.listen(PORT, () => {
-//     console.log(`Now listening for ByShip traffic at port ${PORT}`)
-// })
+app.listen(PORT, () => {
+    console.log(`Now listening for ByShip traffic at port ${PORT}`)
+})
 
 export default app;
 
